ce3fd4397345f3266cc126a3cdc3c29084f0a8c3
frmar440 <frederic.marcotte.2@ulaval.ca>
2022-03-20 16:50:30 -0400

    fix

--- plot_data.py
+++ plot_data.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,6 +14,29 @@ def plot_array(array):
 
 
 if __name__ == '__main__':
+
+    spectrum_non_scarifie = np.load("data/numpy_arrays/spectrum_non_scarifie.npy")
+    spectrum_scarifie = np.load("data/numpy_arrays/spectrum_scarifie.npy")
+    spectrum_background = np.load("data/numpy_arrays/spectrum_background.npy")
+
+    # nmf custom init
+    Hp = np.vstack((spectrum_non_scarifie, spectrum_scarifie, spectrum_background))
+    Hp[Hp<0] = 0
+
+    # Hp figure
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel('wavelength [nm]')
+    ax.set_ylabel('intensity [arb.u.]')
+
+    ax.plot(np.arange(350, 1001), spectrum_scarifie, label="scarified")
+    ax.plot(np.arange(350, 1001), spectrum_non_scarifie, label="non-scarified")
+    ax.plot(np.arange(350, 1001), spectrum_background, label="background")
+
+    ax.tick_params(direction='in')
+
+    plt.legend()
+    plt.show()
 
     fig_data = np.load("data/figures/fig_data.npy")
     table_data = np.load("data/figures/table_data.npy")
@@ -41,14 +65,14 @@ if __name__ == '__main__':
     ax1.set_xlabel(r'$\beta$ [-]')
     ax1.set_ylabel('reconstruction_error [-]', color=color)
     ax1.plot(betas, table_data[:,1], color=color)
-    ax1.tick_params(axis='y', labelcolor=color, direction='in')
+    ax1.tick_params(axis='y', labelcolor=color)
 
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
     color = 'tab:red'
     ax2.set_ylabel('divergence_error [-]', color=color)  # we already handled the x-label with ax1
     ax2.plot(betas, table_data[:,2], color=color)
-    ax2.tick_params(axis='y', labelcolor=color, direction='in')
+    ax2.tick_params(axis='y', labelcolor=color)
 
     ax1.set_xscale('log')
     ax1.set_xticks([0.01, 0.1, 1, 10])
@@ -61,8 +85,7 @@ if __name__ == '__main__':
 
     ax.set_xlabel(r'$\beta$ [-]')
     ax.set_ylabel('sum_errors [-]')
-    ax.plot(betas, table_data[:,1]/max(table_data[:,1]) + table_data[:,2]/max(table_data[:,2]), color='k')
-    ax.tick_params(direction='in')
+    ax.plot(betas, 10*table_data[:,1] + table_data[:,2], color='k')
 
     ax.set_xscale('log')
     ax.set_xticks([0.01, 0.1, 1, 10])
